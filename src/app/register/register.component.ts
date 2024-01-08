63c59cd3d55778c239a5c296ac5b65767ff2c766
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-01-08 17:04:38 -0500

    form added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+    formRegister:FormGroup;
+     constructor(){ 
+    this.formRegister = this.createFormGroup();
+  }
+    createFormGroup(){
+      return new FormGroup({
+        username: new FormControl(),
+        password : new FormControl()
+
+      })
+    }
+    onsubmit(){
+      console.log(this.formRegister.value)
+    }
 
 }
