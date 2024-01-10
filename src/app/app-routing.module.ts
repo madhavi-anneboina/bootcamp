4745e5fece470fe95bc20c8d8df9a912863a0c4e
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-01-10 17:38:23 -0500

    added employees component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 const routes: Routes = [
   
   {path :'', component:HomeComponent},
   {path :'home', component:HomeComponent},
-  {path :'register', component:RegisterComponent, pathMatch:"full"}
+  {path :'register', component:RegisterComponent, pathMatch:"full"},
+  {path :'employees', component:EmployeesComponent, pathMatch:"full"}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
