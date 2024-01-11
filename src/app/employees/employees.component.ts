f3d49004c673ceea48912df29cd1e74127a86e87
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-01-11 17:48:51 -0500

    employee api call added

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit{
+  employeee:any;
+  constructor(private http: HttpClient) {
 
+  }
+ ngOnInit(): void {
+ this.http.get("http://localhost:3000/employees").subscribe(
+  (data) => {
+    this.employeee = data
+    console.log(this.employeee)
+  }
+
+  )
+ 
+ }
 }
