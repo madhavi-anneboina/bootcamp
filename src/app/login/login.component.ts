05aa1b8079ebe251886a48b48a276439ba7ea57e
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-01-15 17:05:03 -0500

    added data

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl,FormGroup,Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent {
   formLogin:FormGroup;
+  user$!:Observable<any>
   constructor(private http: HttpClient){ 
- this.formLogin = this.createFormGroup();
+  this.formLogin = this.createFormGroup();
+ 
 }
  createFormGroup(){
    return new FormGroup({
@@ -20,11 +23,25 @@ export class LoginComponent {
    })
  }
  onsubmit(){
-     this.http.post(
-       "http://localhost:3000/employees",
-       this.formLogin.value
-       )
-       .subscribe(response =>console.log(response));
+  let currentUser = this.formLogin.value.username
+  let currentPassword = this.formLogin.value.password
+   this.user$ = this.http.get( "http://localhost:3000/employees", 
+   {
+    params : {
+      username :currentUser,
+      password :currentPassword
+    }
+   }
+    );
+    this.user$.subscribe(data => {
+     if(currentUser == data[0].username && currentPassword == data[0].password){
+     console.log("Valid User")
+     }  else{
+      console.log("Invalid User")
+     }
+
+    })
+     
  }
 Oninit():void{
 
