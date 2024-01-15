a5bdcc28b5198f83839ad6c68a632e63a9be7c1c
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-01-15 16:25:16 -0500

    login data added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup,Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  formLogin:FormGroup;
+  constructor(private http: HttpClient){ 
+ this.formLogin = this.createFormGroup();
+}
+ createFormGroup(){
+   return new FormGroup({
+     username: new FormControl('',[Validators.required]),
+     password : new FormControl('',[Validators.required])
+
+   })
+ }
+ onsubmit(){
+     this.http.post(
+       "http://localhost:3000/employees",
+       this.formLogin.value
+       )
+       .subscribe(response =>console.log(response));
+ }
+Oninit():void{
+
+}
 
 }
