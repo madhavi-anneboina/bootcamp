17c3ba2fc4b69595127fddb1c5a26043973ef128
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-02-06 20:53:27 -0500

    added ng oninit

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit{
+  userDetails! :FormGroup
+  constructor(){
+  }
+
+    ngOnInit(): void{
+    }
+
+   
+
+   
 
 }
