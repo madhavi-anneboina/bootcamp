f3bb3a495ce490df61386fc647345f9f274996f6
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-02-20 19:37:25 -0500

    added directives data

--- src/app/boldandblue.directive.ts
+++ src/app/boldandblue.directive.ts
@@ -1,16 +1,21 @@
-import { Directive, ElementRef,OnInit,Renderer2} from '@angular/core';
+import { Directive, ElementRef,Input,OnInit,Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBoldandblue]'
 })
 export class BoldandblueDirective implements OnInit {
-
+   
+  @Input() appBoldandblue: string=""
   constructor(private elRef:ElementRef, private renderer:Renderer2) { 
-
   }
   ngOnInit(): void {
+  
     this.renderer.listen(this.elRef.nativeElement,'mouseenter',()=>{
       this.renderer.setStyle(this.elRef.nativeElement,'color',"blue");
+      this.renderer.setStyle(this.elRef.nativeElement,'font-weight',"bold");
+    })
+    this.renderer.listen(this.elRef.nativeElement,'mouseleave',()=>{
+      this.renderer.setStyle(this.elRef.nativeElement,'color',"pink");
       this.renderer.setStyle(this.elRef.nativeElement,'font-weight',"bold");
     })
     
