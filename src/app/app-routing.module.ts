460e1173d000ef4671f4f0edd5ba3ade341812f9
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-02-23 23:38:54 -0500

    save data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { FormBuilderComponent } from './form-builder/form-builder.component';
 import { ComponentstemplatesComponent } from './componentstemplates/componentstemplates.component';
 import { ExcerciseComponent } from './excercise/excercise.component';
 import { ExampleComponent } from './example/example.component';
+import { MyFormComponent } from './my-form/my-form.component';
 
 const routes: Routes = [
   
@@ -21,7 +22,8 @@ const routes: Routes = [
   {path :'form', component:FormBuilderComponent, pathMatch:"full"},
   {path :'components', component:ComponentstemplatesComponent, pathMatch:"full"},
   {path :'excercise', component:ExcerciseComponent, pathMatch:"full"},
-  {path :'example', component:ExampleComponent, pathMatch:"full"}
+  {path :'example', component:ExampleComponent, pathMatch:"full"},
+  {path :'myform', component:MyFormComponent, pathMatch:"full"}
 
 ];
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  userDetails! :FormGroup
+  username:any;
+  password:any;
+  confirmPassword: any;
+  constructor(private fb: FormBuilder) { }
 
+
+  ngOnInit() {
+    this.userDetails = new FormGroup({
+      username: new FormControl(null,[Validators.required,Validators.minLength(8)]),
+      password: new FormControl(null,[Validators.required,Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]),
+      confirmPassword: new FormControl(null,[Validators.required])
+    },
+    // { validator: this.passwordMatchValidator }
+    );
+  }
+
+  // passwordMatchValidator(fg: FormGroup){
+  //   return fg.get('password').value === fg.get('confirmPassword').value
+  //   ? null
+  //   : { mismatch: true };
+  // }
+
+  onSubmit() {
+    if (this.userDetails.valid) {
+      // Perform your submit logic here
+      console.log('Form submitted:', this.userDetails.value);
+    }
+  }
 }
+
+
