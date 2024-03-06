13156530dd4165c6884b5f2f3b13bf15dd31cdb5
Madhavi Anneboina <madhaavi.anneboina@gmail.com>
2024-03-05 21:30:59 -0500

    child to parent communication

--- src/app/expanditem/expanditem.component.ts
+++ src/app/expanditem/expanditem.component.ts
@@ -6,11 +6,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./expanditem.component.css']
 })
 export class ExpanditemComponent {
+  childMessage :string = "Heloooo iam coming from child"
   @Input() data:any
   @Output() toggleDescription = new EventEmitter<void>();
+  @Output() notifyParent : EventEmitter<string> = new EventEmitter<string>();
 
   // onToggleDescription(): void {
   //   this.toggleDescription.emit();
   // }
+
+
+  // Child to parent Communication 
+  sendNotification(): void {
+    this.notifyParent.emit(this.childMessage);
+  }
 
 }
